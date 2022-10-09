28035b3a3e67255981c3763dcaffd1c0842d7c6b
jitendra kumar <jk540499@gmail.com>
2022-10-09 17:49:43 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import { HashRouter, Routes,Route,} from "react-router-dom";
+import Home from './components/Home';
+import Services from './components/Service';
+import About from './components/About';
+import Contect from './components/Contect'
+
+function App() {
+  return (
+    <>
+   <HashRouter>
+   <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/Services" element={<Services/>} />
+      <Route path="/contect" element={<Contect/>} />
+    </Routes>
+  </HashRouter>,
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useState,useEffect} from 'react';
+import '../css/navbar.css'
+import logo from '../images/logo.jpg'
+import {Link} from "react-router-dom";
+import '../fontawesome/css/all.css';
+
+
+function Navbar() {
+
+const [width, setwidth] = useState(window.innerWidth);
+
+function handlewidthchange() {
+    setwidth(window.innerWidth);
+    window.location.reload();
+}
+useEffect(()=>{
+    window.addEventListener('resize',handlewidthchange);
+    return ()=>{
+      window.removeEventListener('resize',handlewidthchange);
+      
+    }
+  },[]);
+
+let smallscreen = width<=650;
+
+function handleclick() {
+ document.querySelector(".iconbtn>i").classList.toggle("fa-xmark");
+
+let list = document.getElementById("list-items");
+list.style.display = list.style.display !== 'grid' ? 'grid' : 'none';
+
+}
+
+  return (
+   <>
+   <nav className="navbar">
+    <img className="logo" src={logo} alt="Logo.."/>
+    
+    <ul id='list-items' className={smallscreen?'smalldevices':'nav-links'}>
+    <li><Link className="link" to="/">Home</Link></li>
+    <li><Link className="link" to="/About">About</Link></li>
+    <li><Link className="link" to="/Services">Services</Link></li>
+    <li><Link className="link" to="/Contect">Contect</Link></li>
+    </ul>
+    <span className='iconbtn' style={smallscreen ? { display:'block'} : {display : 'none'} }>
+         <i className="fa-solid fa-bars" onClick={handleclick} ></i></span>
+    {/* <span><i class="fa-solid fa-xmark"></i></span> */}
+   </nav>
+   </>
+  )
+}
+
+export default Navbar
